2e55b4ba2d3f23beb4a8486b7b90ed51bcd9ebe2
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-01-22 11:08:56 -0500

    add test for naked

---
+++ tests/naked.rs
@@ -0,0 +1,32 @@
+use tinycbor_derive::{Encode, Decode, CborLen};
+use tinycbor::*;
+
+#[derive(Encode, Decode, CborLen)]
+#[cbor(naked, error = "HelloError")]
+pub enum Hello {
+    #[n(0)]
+    World,
+    #[n(3)]
+    People,
+    #[n(2)]
+    Guys,
+    #[n(1)]
+    Folks,
+}
+
+#[derive(Encode, Decode, CborLen)]
+#[cbor(naked)]
+pub struct Wrapper(u64);
+
+#[test]
+fn roundtrip() {
+    let value = Wrapper(42);
+    let encoded = to_vec(&value);
+    let decoded: Wrapper = Decode::decode(&mut Decoder(&encoded)).unwrap();
+    assert_eq!(value, decoded);
+
+    let value = Hello::People;
+    let encoded = to_vec(&value);
+    let decoded: Hello = Decode::decode(&mut Decoder(&encoded)).unwrap();
+    assert_eq!(value, decoded);
+}
