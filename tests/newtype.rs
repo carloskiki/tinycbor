c30be6490a41a0f652572fd7e576716741b01f5d
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-03-13 22:24:20 -0400

    chore: move stuff around

--- tests/newtype.rs
+++ tests/newtype.rs
@@ -1,18 +1,34 @@
 use tinycbor_derive::{Encode, Decode, CborLen};
+use tinycbor::{Decode, Decoder, to_vec};
 
-#[derive(Encode, Decode, CborLen)]
+#[derive(Debug, Encode, Decode, CborLen, PartialEq)]
 struct Struct(u32);
 
-#[derive(Encode, Decode, CborLen)]
+#[derive(Debug, Encode, Decode, CborLen, PartialEq)]
 enum Enum {
     #[n(0)]
     Newtype(u32),
 }
 
-#[derive(Encode, Decode, CborLen)]
+#[derive(Debug, Encode, Decode, CborLen, PartialEq)]
 enum WithOthers {
     #[n(0)]
     Newtype(u32),
     #[n(1)]
     Unit,
+}
+
+#[test]
+fn roundtrip() {
+    let s = Struct(42);
+    let e = Enum::Newtype(42);
+    let w = WithOthers::Newtype(42);
+
+    let s_bytes = to_vec(&s);
+    let e_bytes = to_vec(&e);
+    let w_bytes = to_vec(&w);
+
+    assert_eq!(s, Struct::decode(&mut Decoder(&s_bytes)).unwrap());
+    assert_eq!(e, Enum::decode(&mut Decoder(&e_bytes)).unwrap());
+    assert_eq!(w, WithOthers::decode(&mut Decoder(&w_bytes)).unwrap());
 }
